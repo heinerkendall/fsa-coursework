8fd70a3035b4c41f445c20c73a6a6ae94a6557f4
Kendall Heiner <kendallheiner@Kendalls-MacBook-Air.local>
2024-10-15 19:11:19 -0400

    block10

---
+++ Coursework /block07/workshop.js
@@ -0,0 +1,17 @@
+/*
+*1.) Declare a string veriable and assign it to the value "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+* 2.) Declare and assign 3 variables, one for each number in the combination 
+* 3.) Code 1 will be using addition 
+* 4.) Code 2 will be using subtraction 
+* 5.) Code 3 will be using multiplication 
+* 6.) Display the string and the codes the user 
+
+*/
+
+const userMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+let code1 = 1 + 1
+let code2 = 10 - 5
+let code3 = 7 * 3 
+
+window.alert(userMessage + "\n" + code1 + " " + code2 + " " + code3)
